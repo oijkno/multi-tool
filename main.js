38c11c39a04e6d72cc062cb8f54fcfb5069ede2b
oijkno <132946548+oijkno@users.noreply.github.com>
2023-05-09 10:43:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+var prmpt = prompt('Choose a tool\n (1) Edpuzzle Hack\n (2) Test');
 
+
+function main(){
+var m = document.body.createElement('script');
+m.src = 'https://cdn.jsdelivr.net/gh/oijkno/multi-tool@main/test.js';
+document.body.appendChild(m) 
+}
+
+
+
+if(prmpt === '1'){
+  alert()
+}
+
+else if(prmpt === '2'){
+ main() 
+}
